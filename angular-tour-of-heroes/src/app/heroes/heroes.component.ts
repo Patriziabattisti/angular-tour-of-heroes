433b045aa55ebaf6ab7e055686c834488ef14ba2
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-04-29 15:35:41 +0200

    finished service (with messages)

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +11,10 @@ import {HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
+
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService:MessageService) {
   }
 
   ngOnInit() {
@@ -21,6 +24,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
